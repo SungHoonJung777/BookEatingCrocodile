217c5ea7c9cf667f9a800471dd4cc10cf103f27a
SungHoonJung777 <sshu777@naver.com>
2024-04-26 12:48:28 +0900

    - 이메일 전송 추가

--- src/main/java/org/fullstack4/springmvc/controller/MemberController.java
+++ src/main/java/org/fullstack4/springmvc/controller/MemberController.java
@@ -4,18 +4,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.fullstack4.springmvc.dto.MemberDTO;
 import org.fullstack4.springmvc.service.MemberServiceIf;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.Random;
 
 @Log4j2
 @Controller
@@ -23,6 +25,10 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberServiceIf memberServiceIf;
+
+    @Autowired
+    private JavaMailSender mailSender;
+/*
 
     @GetMapping("/view")
     public void view(//@RequestParam(name="user_id", defaultValue = "") String user_id,
@@ -45,14 +51,58 @@ public class MemberController {
         model.addAttribute("user_id", user_id);
         model.addAttribute("memberDTO", memberDTO);
     }
+*/
 
     @GetMapping("/join")
-    public void joinGET() {
+    public String joinGET() {
         log.info("============================");
         log.info("MemberController >> joinGET()");
         log.info("============================");
+        String url = "/member/joinForm";
+        return url;
     }
+    @GetMapping("/email")
+    @ResponseBody
+    public String  emailGET(String email) throws Exception{
+        log.info("============================");
+        log.info("MemberController >> emailGET()");
+        log.info(email);
+        log.info("============================");
+        Random random = new Random();
+        int checkNum = random.nextInt(888888) + 111111;
+        log.info("인증번호 " + checkNum);
 
+        /* 이메일 보내기 */
+        String setFrom = "sshu777@naver.com";
+        String toMail = email;
+        String title = "회원가입 인증 이메일 입니다.";
+        String content =
+                "홈페이지를 방문해주셔서 감사합니다." +
+                        "<br><br>" +
+                        "인증 번호는 " + checkNum + "입니다." +
+                        "<br>" +
+                        "해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
+
+       /* try {
+
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+            helper.setFrom(setFrom);
+            helper.setTo(toMail);
+            helper.setSubject(title);
+            helper.setText(content,true);
+
+            mailSender.send(message);
+
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }*/
+
+        String num = Integer.toString(checkNum);
+
+        return num;
+    }
 /*
     @PostMapping("/join")
     public String joinPOST(@Valid MemberDTO memberDTO,
